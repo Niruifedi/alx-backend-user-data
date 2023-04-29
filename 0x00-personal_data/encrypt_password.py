945acb3b09eb978aafc120828bbd6004dd96619b
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-04-29 03:36:50 +0100

    validate pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     hash_password method for RedactingFormatter class
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    is_valid method for RedactingFormatter class
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
