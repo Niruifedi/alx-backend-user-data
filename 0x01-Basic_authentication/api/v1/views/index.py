c11b2029ba76c038f91bcfc137b746760fa0127e
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-04 23:57:23 +0100

    403 forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,10 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
+@app_views.route('/unauthorized/', methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
       - abort(401)
     """
     return abort(401)
+
+
+@app_views.route('/forbidden/', methods=["GET"], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - abort(403)
+    """
+    return abort(403)
