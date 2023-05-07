86b143dd368e7aa74117fac58d1fe01e88782812
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-07 03:25:16 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,10 +9,15 @@ class BasicAuth(Auth):
     """
     Basic Authentication Inherits from the Auth Module
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """
+        this public method returns the base64 part of the Authorization header
+        """
+
         if not authorization_header:
             return None
-        
+
         if not isinstance(authorization_header, str):
             return None
 
@@ -20,4 +25,3 @@ class BasicAuth(Auth):
             return authorization_header.split(' ')[1]
         else:
             return None
-       
