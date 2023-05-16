6a66c70f1c436c9c74bd7efe3b29c9712d133c19
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-16 08:46:34 +0100

    new view

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,23 +7,25 @@ import os
 from api.v1.views import app_views
 from models.user import User
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def session_login():
     """
     method for session login
     """
     email = request.form.get('email')
-    pwd = request.form.get('password')
     if email is None or email == "":
         return jsonify({'error': 'email missing'}), 400
+    pwd = request.form.get('password')
     if pwd is None or pwd == "":
         return jsonify({'error': 'password missing'}), 400
-    
+
     users = User.search({"email": email})
+
     if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
     for user in users:
-        if user.is_valid_password(password):
+        if user.is_valid_password(pwd):
             from api.v1.app import auth
             session_id = auth.create_session(user.id)
             resp = jsonify(user.to_json())
